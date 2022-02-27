612e5f405b8c2cbe2efe1ec858720c144ad09f92
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-02-27 14:22:03 +0530

    add countdown timer on egg press

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12];
+    
+    var counter = 0;
+    
+    var timer = Timer();
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate();
+        
+        let hardness = sender.currentTitle!;
+        
+        let result = eggTimes[hardness]!
+        
+        counter = result * 60;
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+        }
+    }
+    
 
 }
