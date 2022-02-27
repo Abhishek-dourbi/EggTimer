d257ca90d4d9478672e58b58384bf552fe37812c
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-02-27 20:44:08 +0530

    add progress view to show the countdown progress

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,28 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720];
+    let eggTimes = ["Soft": 3, "Medium": 500, "Hard": 7];
     
-    var counter = 0;
+    var totalTime = 0;
+    
+    var secondsPassed = 0;
     
     var timer = Timer();
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
         timer.invalidate();
-        
         let hardness = sender.currentTitle!;
+        totalTime = eggTimes[hardness]!;
         
-        counter = eggTimes[hardness]!
+        progressBar.progress = 0;
+        secondsPassed = 0;
+        titleLabel.text = hardness;
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
         
-        if counter > 0 {
-            print("\(counter) seconds")
-            counter -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1;
+            let percentage = Float(secondsPassed) / Float(totalTime);
+            progressBar.progress = percentage;
         } else {
             timer.invalidate();
             titleLabel.text = "DONE!";
